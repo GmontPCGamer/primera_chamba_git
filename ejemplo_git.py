ddcc6cd327899a6cc7d85ad3cf40012c383d157f
pato <p.carvajal@duocuc.cl>
2024-05-29 09:17:02 -0400

    inicio codigo titulo

---
+++ ejemplo_git.py
@@ -0,0 +1,9 @@
+#ejercicio diccionario de hoy: usuarios:
+print('menú')
+ 
+ #git config = global 
+ #git config --global user.name "nombre"
+ #git config --global user.email "correo"
+
+ #git init 
+ #
